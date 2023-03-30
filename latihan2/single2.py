addcfe1c8985188a589a77725287b1b149f0a89c
AliMabrurMubarok <127761433+AliMabrurMubarok@users.noreply.github.com>
2023-03-30 13:24:54 +0700

    Add files via upload

---
+++ latihan2/single2.py
@@ -0,0 +1,23 @@
+#Nama : ALI MABRUR MUBAROK
+#Nim : 210511112
+#Kelas : TIF21C
+
+class Rumahsakit:
+    def __init__(self, nama, alamat):
+        self.nama = nama
+        self.alamat = alamat
+
+    def dirawat(self):
+        print(self.nama, "sedang dirawat")
+
+class Pasien(Rumahsakit):
+    def __init__(self, nama, alamat, penyakit):
+        super().__init__(nama, alamat)
+        self.penyakit = penyakit
+
+    def pulang(self):
+        print(self.nama, "telah dipulangkan")
+
+pasien1 = Pasien("naruto", "konoha", "asamlambung")
+pasien1.dirawat()
+pasien1.pulang()
